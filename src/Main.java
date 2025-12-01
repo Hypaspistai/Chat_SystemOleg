a22306c165ba496add89c0861ac2dc00fc627454
Hypaspistai <shhegolkov1992@mail.ru>
2025-12-02 02:52:23 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.SwingUtilities;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            ChatServer.main(new String[]{});
+        }).start();
+        Thread.sleep(2000);
+        System.out.println("Запуск клиента 1...");
+        SwingUtilities.invokeLater(() -> {
+            new ClientWindow().setTitle("Пользователь 1");
+        });
+        Thread.sleep(2000);
+        System.out.println("Запуск клиента 2...");
+        SwingUtilities.invokeLater(() -> {
+            new ClientWindow().setTitle("Пользователь 2");
+        });
     }
 }
